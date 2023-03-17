a5a294a57d82ce0161e1d6f8dfed0c8b86aac41a
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2023-03-17 18:31:02 +0100

    path Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.search/test/org/gecko/search/BasicLuceneIndexTest.java
+++ org.gecko.search/test/org/gecko/search/BasicLuceneIndexTest.java
@@ -223,7 +223,8 @@ public class BasicLuceneIndexTest {
 			location = basicService.initializeIndexLocation(config);
 			assertNotNull(location);
 			String path = location.getAbsolutePath();
-			assertTrue(path.endsWith("foo/bar/foo") || path.endsWith("foo\bar\foo"));
+			System.err.println(path);
+			assertTrue(path.endsWith("foo/bar/foo") || path.endsWith("foo\\bar\\foo"));
 		} catch (Exception e) {
 			fail("unexpected exception in initializing location");
 		} finally {
